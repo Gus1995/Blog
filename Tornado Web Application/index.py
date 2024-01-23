ee5e555c9043d61097539cd5ffb68a8c3acf8eec
Gus1995 <gustavo.schuster@live.com>
2024-01-23 20:38:04 -0300

    Tornado App

---
+++ Tornado Web Application/index.py
@@ -0,0 +1,19 @@
+import tornado.web
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World")
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", basicRequestHandler),
+
+    ]
+    )
+
+    port = 8882
+    app.listen(port)
+
+    print (f"Application is ready and listening on port {port}")
+    tornado.ioloop.IOLoop.current().start()
