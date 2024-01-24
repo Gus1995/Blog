1e99d6a870df2a5eafcfb720f2d315c43fdcb2dd
Gus1995 <gustavo.schuster@live.com>
2024-01-23 21:25:30 -0300

    Adding html file

--- Tornado Web Application/index.py
+++ Tornado Web Application/index.py
@@ -5,12 +5,34 @@ class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World")
 
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+    def get(self, studentName, courseID):
+        self.write(f"Welcome {studentName}!!! the course you're viewing is {courseID}")
+
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if (num.isdigit()):
+            r = "odd" if int(num) % 2 else "even"
+            self.write(f"The integer {num} is {r}")
+        else:
+            self.write(f"{num} is not a valid integer.")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
+        (r"/Animal", listRequestHandler),
+        (r"/IsEven", queryParamRequestHandler),
+        (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler)
 
-    ]
-    )
+    ])
+
+
 
     port = 8882
     app.listen(port)
